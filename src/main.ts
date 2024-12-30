1dbb1c5c25b93170a4b228b697cddc7681109c18
malav <malav.naagar@imzcorporate.com>
2024-12-30 16:37:43 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*', // or ['http://localhost:3001', 'https://my-frontend.com', etc.]
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, x-api-key',
+    credentials: false, // or true, if you need cookies
+  });
+
   await app.listen(3000);
 }
 bootstrap();
